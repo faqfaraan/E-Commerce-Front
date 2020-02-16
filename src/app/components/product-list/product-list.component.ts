58c2ce2409a2db8986fc75ea22d024b8a39de1b6
Faraan <faqfaraan@gmail.com>
2020-02-15 23:00:07 -0500

    updated item display page & increased api response to 100 items

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,14 +4,15 @@ import { Product } from 'src/app/common/product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
+  // templateUrl: './product-list-table.component.html',
   // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[];
-  
+
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
